9734a5c0d5b5ce2c4640c7a2bd88fce96a3d59e9
Abdelmejid Oumer Ali <abdelmejid89@gmail.com>
2022-07-26 13:55:16 +0300

    Fix duplicates

--- src/js/addComment.js
+++ src/js/addComment.js
@@ -7,15 +7,23 @@ const addComment = () => {
   formEl.addEventListener('submit', async (event) => {
     event.preventDefault();
 
+    const username = usernameEl.value;
+    const comment = commentMsgEl.value;
+
+    if (!username || !comment) return;
+
     await fetch(`${INVOLVEMENT_URL}/comments`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         item_id: formEl.dataset.item,
-        username: usernameEl.value,
-        comment: commentMsgEl.value,
+        username,
+        comment,
       }),
     });
+
+    usernameEl.value = '';
+    commentMsgEl.value = '';
 
     displayComments(formEl.dataset.item);
   });
--- src/js/displayComments.js
+++ src/js/displayComments.js
@@ -3,10 +3,11 @@ import INVOLVEMENT_URL from './config.js';
 const commentsEl = document.querySelector('.all-comments');
 
 const displayComments = (item) => {
+  commentsEl.innerHTML = '<h2 >Comments</h2>';
+
   fetch(`${INVOLVEMENT_URL}/comments?item_id=${item}`)
     .then((response) => response.json())
     .then((data) => {
-      commentsEl.innerHTML = '<h2 >Comments</h2>';
       data.forEach((comment) => {
         const html = `<p>${comment.username} ${comment.comment}</p>`;
         commentsEl.insertAdjacentHTML('beforeend', html);
