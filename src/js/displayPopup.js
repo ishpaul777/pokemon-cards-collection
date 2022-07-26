05ff9f29bf8528d21577359bc7cdd3a00e7f8443
Abdelmejid Oumer Ali <abdelmejid89@gmail.com>
2022-07-26 11:49:56 +0300

    Display detailed information of items

---
+++ src/js/displayPopup.js
@@ -0,0 +1,76 @@
+import { typeColor } from './displayCards.js';
+
+const listsEl = document.querySelector('.card-collection');
+const popupEl = document.querySelector('.details-page');
+
+const displayPopup = () => {
+  listsEl.addEventListener('click', (event) => {
+    const clicked = event.target.closest('.see-details');
+    if (!clicked) return;
+
+    fetch(clicked.dataset.url)
+      .then((response) => response.json())
+      .then((data) => {
+        const pokemonName = data.species.name[0].toUpperCase() + data.species.name.substring(1);
+        const pokeImgURL = data.sprites.other.dream_world.front_default;
+        const locationEncounters = data.location_area_encounters;
+        const statHp = data.stats[0].base_stat;
+        const statAttack = data.stats[1].base_stat;
+        const statDefense = data.stats[2].base_stat;
+        const statSpecialAttack = data.stats[3].base_stat;
+        const statSpecialDefense = data.stats[4].base_stat;
+        const statSpeed = data.stats[5].base_stat;
+
+        const abilitiesEl = document.querySelector('.abilities-pop');
+        abilitiesEl.innerHTML = '';
+        const { types } = data;
+
+        let themeColor;
+        if (types[0].type.name !== 'normal' || types[1] === undefined) {
+          themeColor = typeColor[types[0].type.name];
+        } else {
+          themeColor = typeColor[types[1].type.name];
+        }
+
+        types.forEach((type) => {
+          const formattedType = type.type.name[0].toUpperCase() + type.type.name.slice(1);
+          const ability = document.createElement('span');
+          ability.classList.add('ability');
+          ability.innerHTML = formattedType;
+          ability.style.background = themeColor;
+          abilitiesEl.appendChild(ability);
+        });
+
+        fetch(locationEncounters)
+          .then((response) => response.json())
+          .then((data) => {
+            const locationsEl = document.querySelector('.locations');
+            locationsEl.innerHTML = '<h2>Top 3 Locations</h2>';
+
+            if (data.length === 0) return;
+            for (let i = 0; i < 3; i += 1) {
+              const locationSplitted = data[i].location_area.name.split('-');
+              const location = locationSplitted
+                .map((loc) => loc[0].toUpperCase() + loc.slice(1))
+                .join(' ');
+
+              const html = `<h3 class="location">${location}</h3>`;
+              locationsEl.insertAdjacentHTML('beforeend', html);
+            }
+          });
+
+        document.querySelector('.modal-image').src = pokeImgURL;
+        document.querySelector('.pokemon-name').textContent = pokemonName;
+        document.querySelector('.hp-score--pop h3 .score').textContent = statHp;
+        document.querySelector('.attack-score--pop h3 .score').textContent = statAttack;
+        document.querySelector('.defense-score--pop h3 .score').textContent = statDefense;
+        document.querySelector('.spl-attack-score--pop h3 .score').textContent = statSpecialAttack;
+        document.querySelector('.spl-defense-score--pop h3 .score').textContent = statSpecialDefense;
+        document.querySelector('.speed-score--pop h3 .score').textContent = statSpeed;
+      });
+
+    popupEl.style.display = 'flex';
+  });
+};
+
+export default displayPopup;
