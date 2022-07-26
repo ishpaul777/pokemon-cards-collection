2006a6d268b56600caca23443e3037fe7419d994
Abdelmejid Oumer Ali <abdelmejid89@gmail.com>
2022-07-26 14:07:37 +0300

    Add comments counter

--- src/js/displayComments.js
+++ src/js/displayComments.js
@@ -3,11 +3,13 @@ import INVOLVEMENT_URL from './config.js';
 const commentsEl = document.querySelector('.all-comments');
 
 const displayComments = (item) => {
-  commentsEl.innerHTML = '<h2 >Comments</h2>';
+  commentsEl.innerHTML = '<h2 class="comments-counter">Comments</h2>';
 
   fetch(`${INVOLVEMENT_URL}/comments?item_id=${item}`)
     .then((response) => response.json())
     .then((data) => {
+      document.querySelector('.comments-counter').innerHTML = `Comments (${data.length || 0})`;
+
       data.forEach((comment) => {
         const html = `<p>${comment.username} ${comment.comment}</p>`;
         commentsEl.insertAdjacentHTML('beforeend', html);
